df42021867973b8a42778ef7a51379c01fb2b8aa
khanhmanhdsz <103480126+justviet1234@users.noreply.github.com>
2022-04-25 08:51:57 +0700

    fix bug login logout

--- Assgn_Viet_Prj301/src/java/controller/LoginController.java
+++ Assgn_Viet_Prj301/src/java/controller/LoginController.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+
 import dal.AcountDBContext;
 import java.io.IOException;
 import java.io.PrintWriter;
--- Assgn_Viet_Prj301/src/java/controller/LogoutControl.java
+++ Assgn_Viet_Prj301/src/java/controller/LogoutControl.java
@@ -5,6 +5,7 @@
  */
 package controller;
 
+
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
